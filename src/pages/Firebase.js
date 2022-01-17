d4ccdea72033e83d6f7a47a8fb2ef01e4bd4c8bd
Nazrawi Demeke <dnazrawi@gmail.com>
2022-01-17 07:27:21 +0300

    fix firebase authentication

--- src/pages/Firebase.js
+++ src/pages/Firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithRedirect, signInWithPopup, signOut } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANfoLDGZsVAl8NwHJUlgLrUynJJMaSsg0",
-  authDomain: "auth-8b1ef.firebaseapp.com",
-  projectId: "auth-8b1ef",
-  storageBucket: "auth-8b1ef.appspot.com",
-  messagingSenderId: "445273762769",
-  appId: "1:445273762769:web:81f403cae7cb5fe3760ef0",
+  apiKey: "AIzaSyDmeTpukDdm8xhLBS-dWcAFp4IF4MuXAAo",
+  authDomain: "snap-b30b7.firebaseapp.com",
+  projectId: "snap-b30b7",
+  storageBucket: "snap-b30b7.appspot.com",
+  messagingSenderId: "268904570912",
+  appId: "1:268904570912:web:36fe848cfa4dc9888d9c45"
 };
 
 const app = initializeApp(firebaseConfig);
