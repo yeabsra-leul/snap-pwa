8640c82e128c53223fefc4801b7fb97a89e01a54
Nazrawi Demeke <dnazrawi@gmail.com>
2022-01-21 01:16:29 +0300

    add exit button on sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
-import '../styles/Sidebar.css';
-import humburger from "../images/humburger.svg"
+import '../styles/sidebar.css';
+import humburger from "../images/humburger.svg";
+import exit from "../images/exit.svg";
 import {Link} from 'react-router-dom';
 import ImageButton from "./Buttons";
 import settings from "../images/settings.svg";
@@ -10,7 +11,7 @@ import groups from "../images/groups.svg";
 
 export default props => {   // TODO: Change URIs, Prevent overlay scrolling behind sidebar
   return (
-    <Menu width={'275px'} customBurgerIcon={<img src={humburger}/>} disableAutoFocus >
+    <Menu width={'275px'} customBurgerIcon={<img src={humburger}/>} customCrossIcon={<img src={exit}/>} disableAutoFocus >
       <Link to="/">
         <ImageButton image={groups} cls={"hamburger-item"} alt={"Groups"}>
           Groups
